01210da0284e756b56d0ec7af3677d1d84ab1cb7
Süleyman Tüysüzoğlu <rock.fear@windowslive.com>
2026-04-12 23:45:36 +0300

    ref

--- src/Modules/Identity/Consumers/CustomerAccountProvisionRequestedConsumer.cs
+++ src/Modules/Identity/Consumers/CustomerAccountProvisionRequestedConsumer.cs
@@ -23,8 +23,6 @@ public sealed class CustomerAccountProvisionRequestedConsumer : IConsumer<Custom
     {
         var message = context.Message;
 
-        var ApiKey = "12345678"; // Generate a secure temporary password in production
-
         var existing = await _userManager.FindByEmailAsync(message.Email);
         if (existing is not null)
         {
