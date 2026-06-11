7d2db104e9e47e7f81733354030c116693b87511
BruteDeepForce <BruteDeepForce@users.noreply.github.com>
2026-06-11 19:06:26 +0000

    deploy: 3b7fa154e24e4dbbc4f21a2e3d5ea1b0debb2d54

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1622290042" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1046650722" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
